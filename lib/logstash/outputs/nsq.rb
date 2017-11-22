d5e1dbddb69d41b8e1efaba2a086bc91b828d9a8
Matthias Schneider <ms@wck.biz>
2017-11-22 15:30:46 +0100

    code cleanup simpler. memory bug seems fixed

--- lib/logstash/outputs/nsq.rb
+++ lib/logstash/outputs/nsq.rb
@@ -1,14 +1,12 @@
 require 'logstash/namespace'
 require 'logstash/outputs/base'
-require 'java'
+require 'nsq'
 
 class LogStash::Outputs::Nsq < LogStash::Outputs::Base
-    declare_threadsafe!
     config_name 'nsq'
 
     default :codec, 'json'
 
-    default :codec, 'json'
     config :nsqd, :validate => :string, :default => nil
     config :nsqlookupd, :validate => :array, :default => nil
     config :topic, :validate => :string, :required => true
@@ -18,9 +16,6 @@ class LogStash::Outputs::Nsq < LogStash::Outputs::Base
  
   public
   def register
-    @thread_batch_map = Concurrent::Hash.new
-
-    require 'nsq'
     options = {
         :nsqlookupd => @nsqlookupd,
         :topic => @topic,
@@ -62,41 +57,15 @@ class LogStash::Outputs::Nsq < LogStash::Outputs::Base
 
     @producer = Nsq::Producer.new(options)
     @logger.info('Registering nsq producer', :nsqd => @nsqd, :nsqlookupd => @nsqlookupd, :topic => @topic)
-    @codec.on_event do |event, data|
-        write_to_nsq(event, data)
-    end
   end # def register
 
-  def write_to_nsq(event, data)
-    begin
-      @producer.write(event.sprintf(data))
-    rescue LogStash::ShutdownSignal
-      @logger.info('nsq producer got shutdown signal')
-    rescue => e
-      @logger.warn('nsq producer threw exception, restarting', :exception => e)
-    end # begin
+  def write_to_nsq(payload)
+    @producer.write(payload)
   end # def send_to_nsq
 
-  def prepare(record)
-    # This output is threadsafe, so we need to keep a batch per thread.
-    @thread_batch_map[Thread.current].add(record)
-  end
-
-  def multi_receive(events)
-    t = Thread.current
-    if !@thread_batch_map.include?(t)
-      @thread_batch_map[t] = java.util.ArrayList.new(events.size)
-    end
-
-    events.each do |event|
-      break if event == LogStash::SHUTDOWN
-      @codec.encode(event)
-    end
-
-    batch = @thread_batch_map[t]
-    if batch.any?
-      retrying_send(batch)
-      batch.clear
+  def multi_receive_encoded(encoded)
+    encoded.each do |event,data|
+      write_to_nsq(data)
     end
   end
 
